27d19deaff2e4667d48b31527c196b2c27dc7f90
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2025-10-21 00:27:41 +0700

    added inheritance from product named clothing

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -34,6 +34,25 @@ class Product {
   getPrice() {
     return `$${formatCurrency(this.priceCents)}`;
   }
+
+  extraInfoHTML() {
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails){
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target = "_blank">Size Chart</a>
+    `;
+  }
 }
 
 export const products = [
@@ -696,5 +715,8 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
+  if(productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
