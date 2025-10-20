c3b13bb11e8f385224f9e4974d377357bb4c9f03
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2025-10-20 20:17:44 +0700

    added cart class

---
+++ javascript-amazon-project-main/data/cart-class.js
@@ -0,0 +1,75 @@
+class Cart{
+    cartItems;
+    localStorageKey;
+
+    constructor(localStorageKey) {
+        this.localStorageKe = localStorageKey;   
+        this.LoadFromStorage();
+    }   
+
+    LoadFromStorage() {
+        this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey)) || [];
+    }
+    
+    saveToStorage() {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    }
+
+    addToCart(productId) {
+        let matchingItem;
+
+        this.cartItems.forEach((cartItem) => {
+            if(cartItem.productId === productId){
+                matchingItem = cartItem;
+            }
+        });
+
+        if(matchingItem){
+            matchingItem.quantity += 1;
+        }
+        else{
+            this.cartItems.push({
+                productId,
+                quantity: 1,
+                deliveryOptionId: '1'
+            });
+        }
+        
+        this.saveToStorage();
+    }
+
+    removeFromCart(productId) {
+        const newCart = [];
+
+        this.cartItems.forEach((cartItem) => {
+            if(cartItem.productId != productId){
+                newCart.push(cartItem);
+            }
+        })
+
+        this.cartItems = newCart;
+        this.saveToStorage();
+    }
+
+    updateDeliveryOption(productId, deliveryOptionId) {
+        let matchingItem;
+
+        this.cartItems.forEach((cartItem) => {
+            if(cartItem.productId === productId){
+                matchingItem = cartItem
+            }
+        });
+
+        matchingItem.deliveryOptionId = deliveryOptionId;
+        this.saveToStorage();
+    }
+}
+
+const cart = new Cart('cart-oop');
+const bussinessCart = new Cart('cart-bussiness');
+
+
+
+console.log(cart);
+console.log(bussinessCart);
+console.log(bussinessCart instanceof Cart);
