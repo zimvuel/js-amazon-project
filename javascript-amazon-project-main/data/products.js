95fd6512c466009dd03b8fb99d67e128a6e790ef
Jonathan Hanska Susanto <jojo12ku@gmail.com>
2025-10-22 12:26:42 +0700

    convert load product from xhr to fetch

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -57,6 +57,27 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if(productDetails.type === 'clothing') {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    }); 
+  });
+
+  return promise;
+}
+
+// loadProductsFetch().then(() => {
+
+// })
+
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
